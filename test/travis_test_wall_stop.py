a7596dff98703a6f45d79bb015aaad6a9baf01fe
Shuro Nakajima <Shuro Nakajima>
2017-11-25 05:09:51 +0900

    fix

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -4,6 +4,19 @@ import rosnode, rospy
 import time
 
 class WallStopTest(unittest.TestCase):
+    def setUp(self):
+	self.count=0
+	rospy.Subscriber('/lightsensors', LightSensorValues, self.callback)
+	self.values=LightSensorValues()
+
+    def callback(self,data):
+	self.count+=1
+	self.values=data
+
+    def test_node_exist(self):
+        nodes=rosnode.get_node_names()
+	self.assertIn('/lightsensors',nodes,"node does not exist")
+
     def set_and_get(self, lf, ls, rs, rf):
         with open("/dev/rtlightsensor0","w") as f:
             f.write("%d %d %d %d\n" % (rf, rs, ls, lf))
