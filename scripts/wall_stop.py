45d2687724aed4aaac56e81798a107617be469ea
Shuro Nakajima <Shuro Nakajima>
2017-11-25 10:17:11 +0900

    fix

--- scripts/wall_stop.py
+++ scripts/wall_stop.py
@@ -26,8 +26,9 @@ class WallStop():
 
 if __name__=='__main__':
     rospy.init_node('wall_stop',log_level=rospy.INFO)
+    w=WallStop()
     rospy.wait_for_service('/motor_on')
     rospy.wait_for_service('/morot_off')
     rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
     rospy.ServiceProxy('/motor_on',Trigger).call()
-    WallStop().run()
+    w.run()
