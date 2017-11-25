3e90277de268dce3421f0a97ecdaff149a25f745
Shuro Nakajima <Shuro Nakajima>
2017-11-25 10:28:10 +0900

    fix

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -25,10 +25,10 @@ class WallStopTest(unittest.TestCase):
 
         with open("/dev/rtmotor_raw_l0","r") as lf,\
              open("/dev/rtmotor_raw_r0","r") as rf:
-#            left = int(lf.readline().rstrip())
-#            right = int(rf.readline().rstrip())
-            left = 100
-            right = 200
+            left = int(lf.readline().rstrip())
+            right = int(rf.readline().rstrip())
+#            left = 100
+#            right = 200
             
         return left, right
 
@@ -38,13 +38,13 @@ class WallStopTest(unittest.TestCase):
 
     def test_io(self):
         left, right = self.set_and_get(400, 100, 100, 0) #total:600
-#        self.assertTrue(left == 0 and right == 0, "can't stop")
+        self.assertTrue(left == 0 and right == 0, "can't stop")
 
         left, right = self.set_and_get(400, 0, 0, 99) #total:499
-#        self.assertTrue(left != 0 and right != 0, "cant't move again")
+        self.assertTrue(left != 0 and right != 0, "cant't move again")
 
         left, right = self.set_and_get(150, 0, 200, 150) #total:500
-#        self.assertTrue(left == 0 and right == 0, "can't stop")
+        self.assertTrue(left == 0 and right == 0, "can't stop")
 
 if __name__=='__main__':
     time.sleep(3)
